abdb62945607b46bcd5d356d96fb591751159f02
ambroseahawo <ahawoadeya995@gmail.com>
2021-12-31 10:45:45 +0300

    list copy

---
+++ 3.containers/1. list methods/3. copy.py
@@ -0,0 +1,7 @@
+fruits = ['apple', 'banana', 'cherry']
+
+print("\nOriginal fruits list: {0}\n".format(fruits))
+
+# copy() returns a copy of the list
+fruits_copy = fruits.copy()
+print("Fruits list copy: {0}".format(fruits_copy))
