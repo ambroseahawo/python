3b4347f7cb8e3ae3f73f1ee288f5f9c988f9cbfa
ambroseahawo <ahawoadeya995@gmail.com>
2021-12-31 10:52:59 +0300

    list count

---
+++ 3.containers/1. list methods/4. count.py
@@ -0,0 +1,7 @@
+fruits = ['apple', 'banana', 'cherry', 'cherry']
+
+print("\nOriginal fruits list: {0}\n".format(fruits))
+
+# count() returns the number of elements with the specified value
+element_count = fruits.count("cherry")  # takes exactly one argument
+print("Number of times value appeared: {0}".format(element_count))
