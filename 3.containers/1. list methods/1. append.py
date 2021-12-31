2904267d415c268ed5178f0c9fc80845e5019bdd
ambroseahawo <ahawoadeya995@gmail.com>
2021-12-31 10:40:40 +0300

    list append

--- 3.containers/1. list methods/1. append.py
+++ 3.containers/1. list methods/1. append.py
@@ -3,6 +3,6 @@ fruits = ['apple', 'banana', 'cherry']
 print("\nOriginal fruits list: {0}\n".format(fruits))
 
 # append() ads an element at the end of the list
-fruits.append('orange')
+fruits.append('orange') # takes one argument
 # append() is a mutator method - the data state is changed
 print("New fruits list: {0}".format(fruits))
