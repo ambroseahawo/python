f167206a5edcff08b4d395ea4b5ff4ae491a4a86
ambroseahawo <ahawoadeya995@gmail.com>
2021-12-31 13:28:33 +0300

    list index

---
+++ 3.containers/1. list methods/6. index.py
@@ -0,0 +1,7 @@
+fruits = ['apple', 'banana', 'cherry', 'cherry']
+
+print("\nOriginal fruits list: {0}\n".format(fruits))
+
+# index() returns the index of the first element with specified value
+index_of_value = fruits.index("banana")
+print("Index of banana in fruits: {0}".format(index_of_value))
