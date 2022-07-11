8c783ef499e09d9e6b7c35fe74a4977c41782e67
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-11 20:07:50 +0300

    gpa-calc-edit

--- CH01/1.fragments/1.1.2 - gpa_calculator.py
+++ CH01/1.fragments/1.1.2 - gpa_calculator.py
@@ -1,31 +1,33 @@
+"""Example illustration of while loop"""
+
 print('Welcome to the GPA calculator.')
 print('Please enter all your letter grades, one per line')
 print('Enter a blank line to designate the end')
 
 # map from letter grades to point value
 points = { 'A+' :4.0, 'A' :4.0, 'A-' :3.67, 'B+' :3.33, 'B' :3.0,
-             'B-' :2.67,'C+' :2.33, 'C' :2.0, 'C' :1.67, 'D+' :1.33,
+             'B-' :2.67,'C+' :2.33, 'C-' :2.0, 'C' :1.67, 'D+' :1.33,
               'D' :1.0, 'F' :0.0 }
 
-num_courses = 1
-total_points = 0
-done = False
+NUM_COURSES = 1
+TOTAL_POINTS = 0
+DONE = False
 
-while not done:
-    print('Course {0}:'.format(num_courses))
+while not DONE:
+    print(f"Course {0}:".format(NUM_COURSES))
     grade = input()
     if grade == '':
-        done = True
-        num_courses -= 1
+        DONE = True
+        NUM_COURSES -= 1
     elif grade not in points:
-        print("Unknown grade '{0}' being ignored".format(grade))
+        print(f"Unknown grade '{0}' being ignored".format(grade))
     else:
-        num_courses += 1
-        total_points += points[grade]
+        NUM_COURSES += 1
+        TOTAL_POINTS += points[grade]
 
-if num_courses > 0:
-    print('Calculating GPA for {0} courses entered.'.format(num_courses))
-    print('Your GPA is {0:.3}'.format(total_points/num_courses))
+if NUM_COURSES > 0:
+    print(f"Calculating GPA for {0} courses entered.".format(NUM_COURSES))
+    print(f"Your GPA is {0:.3}".format(TOTAL_POINTS/NUM_COURSES))
 
 # Illustrate immutability
 # Mutable objects are type list, dict and set
