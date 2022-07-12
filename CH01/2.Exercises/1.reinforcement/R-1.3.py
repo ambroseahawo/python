ea94211edaff88ef19b3267c171e5f699aeafc3a
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-12 16:23:08 +0300

    R-1.3.py edit

--- CH01/2.Exercises/1.reinforcement/R-1.3.py
+++ CH01/2.Exercises/1.reinforcement/R-1.3.py
@@ -1,10 +1,11 @@
-# Write a short Python function, minmax(data), that takes a sequence of
-# one or more numbers, and returns the smallest and largest numbers, in the
-# form of a tuple of length two. Do not use the built-in functions min or
-# max in implementing your solution.
+"""Write a short Python function, min_max(data), that takes a sequence of
+one or more numbers, and returns the smallest and largest numbers, in the
+form of a tuple of length two. Do not use the built-in functions min or
+max in implementing your solution."""
 
 
-def minmax(data):
+def min_max(data):
+    """implementation"""
     smallest = data[0]
     largest = data[0]
 
@@ -18,6 +19,27 @@ def minmax(data):
 
     return (smallest, largest)
 
+# populating list method 1
+print("")
+print("Populating list method 1")
+data_input = []
+number_of_integers = int(input("Enter number of elements: "))
 
-smallest_largest = minmax([14, 43, 27, 9, 39])
-print(smallest_largest)
+for values in enumerate(range(0, number_of_integers)):
+    element = int(input("Enter integer {0}: ".format(values[0])))
+
+    data_input.append(element)
+
+print(data_input)
+
+
+print(min_max(data_input))
+
+# populating list method 2
+print("")
+print("Populating list method 2")
+data_input2 = [int(integer) for integer in input("Enter a string of numbers: ")]
+
+print("Confirm integers in array: {0}".format(data_input2))
+
+print(min_max(data_input2))
