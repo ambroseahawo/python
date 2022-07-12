bcb7bed560e2c57c8a2ed292ef6097e3e4257abd
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-12 17:29:09 +0300

    R-1.8.py edit

--- CH01/2.Exercises/1.reinforcement/R-1.8.py
+++ CH01/2.Exercises/1.reinforcement/R-1.8.py
@@ -1,10 +1,12 @@
 """Python allows negative integers to be used as indices into a sequence,
-such as a string. If string s has length n, and expression s[k] is used for index −n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references
-the same element?"""
+such as a string. If string s has length n, and expression s[k] is used
+for index −n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j]
+references the same element?"""
 
-string_s = "Sample String"
-k = -2
-print('n[k]: {0}'.format(string_s[k]))
+STRING_S = "Sample String"
+print("Length: {0}".format(len(STRING_S)))
+k = -5
+print('s[k]: {0}'.format(STRING_S[k]))
 
-j = k + len(string_s)
-print('n[j]: {0}'.format(string_s[j]))
+j = k + len(STRING_S)
+print('s[j]: {0}'.format(STRING_S[j]))
