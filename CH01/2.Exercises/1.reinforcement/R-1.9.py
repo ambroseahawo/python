a83bc3dd4ab977627e0bf93bebf28e66de2b7389
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-12 17:07:18 +0300

    R-1.9.py edit

--- CH01/2.Exercises/1.reinforcement/R-1.9.py
+++ CH01/2.Exercises/1.reinforcement/R-1.9.py
@@ -1,7 +1,7 @@
-# What parameters should be sent to the range constructor, to produce a
-# range with values 50, 60, 70, 80?
+"""What parameters should be sent to the range constructor, to produce a
+range with values 50, 60, 70, 80?"""
 
-# paython range(start,stop[,step])
+# python range(start,stop[,step])
 
 for values in range(50, 80+1, 10):
     print(values)
