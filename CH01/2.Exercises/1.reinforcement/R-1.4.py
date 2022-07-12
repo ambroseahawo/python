ebae81e63f3ecf22c2c50a60633c61b7bc9a5a36
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-12 16:56:45 +0300

    R-1.4.py edit

--- CH01/2.Exercises/1.reinforcement/R-1.4.py
+++ CH01/2.Exercises/1.reinforcement/R-1.4.py
@@ -1,24 +1,21 @@
-# Write a short Python function that takes a positive integer n and returns
-# the sum of the squares of all the positive integers smaller than n
+"""Write a short Python function that takes a positive integer n and returns
+the sum of the squares of all the positive integers smaller than n"""
 
-def sum_squares(n):
+def sum_squares(integer: int):
+    """implementation"""
     # initialise empty list to store the squares
     squares = []
-    # initialise initial sum as zero
-    total_sum = 0
 
     # iterate from one to n exclusive
-    for ele in range(1, n):
+    for ele in range(1, integer):
         # get the square of eah element
         squares.append(ele*ele)
-    
-    # iterate through the squares
-    for ele in squares:
-        # add each element to the total_sum
-        total_sum += ele
-    
-    return total_sum # return sum of the squares
 
+    return sum(squares) # return sum of the squares
 
-squares_sum = sum_squares(5)
-print(squares_sum)
+integer_input = int(input("Enter a number: "))
+while integer_input < 2:
+    integer_input = int(input("Please enter a number greater than 1: "))
+
+squares_sum = sum_squares(integer_input)
+print("Squares sum: {0}".format(squares_sum))
