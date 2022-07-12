8e8c2ecfc681abda65d9184df9da5001016c3a44
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-12 17:00:33 +0300

    R-1.5.py edit

--- CH01/2.Exercises/1.reinforcement/R-1.5.py
+++ CH01/2.Exercises/1.reinforcement/R-1.5.py
@@ -1,12 +1,18 @@
-# Give a single command that computes the sum from Exercise R-1.4, 
-# relying on Python’s comprehension syntax and the built-in sum function
+"""Give a single command that computes the sum from Exercise R-1.4, 
+relying on Python’s comprehension syntax and the built-in sum function"""
 
-def sum_squares(n):
+def sum_squares(integer: int):
+    """implementation"""
     # list comprehension
-    squares = [number * number for number in range(1, n)]
+    squares = [number * number for number in range(1, integer)]
 
     # return sum of squares list
     return sum(squares)
 
-squares_sum = sum_squares(5)
-print(squares_sum)
+
+integer_input = int(input("Enter a number: "))
+while integer_input < 2:
+    integer_input = int(input("Please enter a number greater than 1: "))
+
+squares_sum = sum_squares(integer_input)
+print("Squares sum: {0}".format(squares_sum))
