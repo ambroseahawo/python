b3c164573406f0d8ae1949dd49e138d8fafb247e
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-07-23 22:27:08 +0300

    C-1.14

--- CH01/2.Exercises/2.Creativity/C-1.14.py
+++ CH01/2.Exercises/2.Creativity/C-1.14.py
@@ -9,6 +9,8 @@ product is odd.
     can form a pair with every other unique element including itself.
 """
 
+from itertools import combinations
+
 def distinct_pairs_count(arr, n):
     """implementation"""
     s = set()
@@ -21,15 +23,39 @@ def distinct_pairs_count(arr, n):
     return count
 
 
+def get_distinct_pairs(arr):
+    """implementation"""
+    # distinct_pairs_arr = [", ".join(map(set, comb)) for comb in combinations(arr, 2)]
+    distinct_pairs_arr = [set(comb) for comb in combinations(arr, 2)]
+    return distinct_pairs_arr
+
 # driver code
 if __name__ == "__main__":
+    # determine number of elements in the array
     array_count = int(input("Enter the number of array elements: ").strip())
+    # initialize the array as empty
     input_arr = []
 
+    # populate the array form user input
     for values in enumerate(range(0, array_count)):
         array_ele = int(input("Enter array element {0}: ".format(values[0])).strip())
         input_arr.append(array_ele)
 
     print("Original array: {0}".format(input_arr))
 
-    print("Distinct pairs: {0}".format(distinct_pairs_count(input_arr, len(input_arr))))
+    # get distinct pairs
+    distinct_pairs = get_distinct_pairs(input_arr)
+    print("Distinct pairs: {0}".format(distinct_pairs))
+
+    # counter odd product pairs
+    COUNTER = 0
+
+    for each_set in distinct_pairs:
+        # get product result
+        RESULT = 1
+        for each_item in each_set:
+            RESULT *= each_item
+        if RESULT % 2 != 0:
+            COUNTER += 1
+
+    print("Number of odd product pairs: {0}".format(COUNTER))
