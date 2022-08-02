a1631c8730a3a43529f7135102691d4f9d045366
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-02 05:05:51 +0300

    C-1.24

--- CH01/2.Exercises/2.Creativity/C-1.24.py
+++ CH01/2.Exercises/2.Creativity/C-1.24.py
@@ -3,13 +3,26 @@ Write a short Python function that counts the number of vowels in a given
 character string.
 """
 
+# import module
+from collections import Counter
+
 def count_vowels(arr_chars):
     """implementation"""
-    print()
+    vowels_list = ['a', 'e', 'i', 'o', 'u']
+    counter = 0
+
+    all_unique_occurrences = Counter(arr_chars)
+
+    for each_vowel in vowels_list:
+        counter += all_unique_occurrences[each_vowel]
+
+    return counter
 
 
 if __name__ == "__main__":
+    print()
     # specify array length
-    char_str = input("Enter a character string: ").strip()
+    input_char_str = input("Enter a character string: ").strip()
+    char_str = input_char_str.lower()
 
-    print(list(char_str))
+    print("Number of vowels in \"{0}\": {1}".format(input_char_str ,count_vowels(list(char_str))))
