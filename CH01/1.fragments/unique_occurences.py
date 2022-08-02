d31bb66b3f0f1dbc1da555b51a997c1e34c7d2bb
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-02 20:30:50 +0300

    unique-occurrences

--- CH01/1.fragments/unique_occurences.py
+++ CH01/1.fragments/unique_occurences.py
@@ -2,12 +2,6 @@
 Get count of all unique occurrences in a list
 """
 
-def count_occurrences(char_arr):
-    """implementation"""
-    print()
-
-
-if __name__ == "__main__":
-    # input character string
-    input_str = input("Enter a string of characters: ").strip()
-    print("Number of vowels in \"{0}\": ".format(input_str))
+def count_occurrences(arr, item):
+    """implementation""" 
+    return arr.count(item)
