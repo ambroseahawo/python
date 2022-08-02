b3d629d6b7510c9c622670109651fb1c7f9bc9a8
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-02 04:46:01 +0300

    C-1.24

---
+++ CH01/2.Exercises/2.Creativity/C-1.24.py
@@ -0,0 +1,15 @@
+"""
+Write a short Python function that counts the number of vowels in a given
+character string.
+"""
+
+def count_vowels(arr_chars):
+    """implementation"""
+    print()
+
+
+if __name__ == "__main__":
+    # specify array length
+    char_str = input("Enter a character string: ").strip()
+
+    print(list(char_str))
