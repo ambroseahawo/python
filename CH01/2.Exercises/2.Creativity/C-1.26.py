d820784dc66fc0e18532328326249c60628b1223
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-03 20:57:02 +0300

    C-1.26

--- CH01/2.Exercises/2.Creativity/C-1.26.py
+++ CH01/2.Exercises/2.Creativity/C-1.26.py
@@ -8,7 +8,18 @@ formula (in the given order), like “a+b = c,” “a = b−c,” or “a ∗ b
 int_1 = int(input("Enter first integer: ").strip())
 int_2 = int(input("Enter second integer: ").strip())
 int_3 = int(input("Enter third integer: ").strip())
+print()
 
 print("Integers entered as follows First: {0}, Second: {1}, Third: {2}".format(
     int_1, int_2, int_3
 ))
+print()
+
+print("Testing the following:")
+print("{0} + {1} = {2}".format(int_1, int_2, int_3))
+print("{0} = {1} - {2}".format(int_1, int_2, int_3))
+print("{0} * {1} = {2}".format(int_1, int_2, int_3))
+print()
+
+# check
+print(bool((int_1 + int_2 == int_3) and (int_1 == int_2 - int_3) and (int_1 * int_2 == int_3)))
