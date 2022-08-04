88190d0ece79f8969021ff57f892dd7f43c0ca4e
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-04 22:08:52 +0300

    combinations

---
+++ CH01/1.fragments/combinations.py
@@ -0,0 +1,6 @@
+"""
+Get all possible strings formed by using
+the characters exactly once
+
+https://stackoverflow.com/questions/464864/how-to-get-all-possible-combinations-of-a-list-s-elements
+"""
