1f4aad1131482db5f146567448c817b668c26f72
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-04 22:34:57 +0300

    P-1.30

--- CH01/3.Projects/P-1.30.py
+++ CH01/3.Projects/P-1.30.py
@@ -9,11 +9,14 @@ GREATER_THAN_2 = False
 REMAINDER_LESS_2 = False
 COUNTER = 0
 
+# do a while loop until the user enters an integer greater than 2
 while not GREATER_THAN_2:
+    print()
     INT_INPUT = int(input("Enter an integer greater than 2: "))
     if INT_INPUT > 2:
         GREATER_THAN_2 = True
 
+    # do a while loop dividing the integer by 2 until the dividend is less than 2
     while not REMAINDER_LESS_2:
         INT_INPUT /= 2
         if INT_INPUT > 2:
