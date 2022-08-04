9406443d7f2b9d206cd61e17c08b975da7badd65
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-04 22:07:04 +0300

    P-1.29

--- CH01/3.Projects/P-1.29.py
+++ CH01/3.Projects/P-1.29.py
@@ -12,15 +12,17 @@ def all_possible_combinations(arr):
     """implementation"""
     for item in range(0, len(arr)+1):
         for subset in itertools.combinations(arr, item):
-            print(subset)
+            if list(subset):
+                print("".join(list(subset)))
 
 # driver code
 if __name__ == "__main__":
     # default array
     DEFAULT_ARRAY = ['c', 'a', 't', 'd', 'o', 'g']
-    print("All Possible strings formed by using the characters: ", end=' ')
+    print("All Possible strings formed by using the characters: ", end='')
     for index,value in enumerate(DEFAULT_ARRAY):
         if index == len(DEFAULT_ARRAY) - 1:
             print(value)
         else:
             print(value, end=',')
+    all_possible_combinations(DEFAULT_ARRAY)
